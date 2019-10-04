1212a9a63595b198bc402455c6e4ad097aec46cb
user <user@users-MacBook-Air.local>
2019-10-04 11:51:10 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.sidebar');
-const content = document.querySelector('.content-wrapper');
-const header = document.querySelector('.header');
-const headerWrapper = document.querySelector('.header-whitebar');
-const sidebarListItems = document.querySelectorAll('.sidebar__list-item');
-const allWindows = document.querySelectorAll('section');
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".sidebar");
+const content = document.querySelector(".content-wrapper");
+const header = document.querySelector(".header");
+const headerWrapper = document.querySelector(".header-whitebar");
+const sidebarListItems = document.querySelectorAll(".sidebar__list-item");
+const allWindows = document.querySelectorAll("section");
+const sidebarOverlay = document.querySelector(".sidebar-overlay");
 
 const hamburgerClickHandler = () => {
-  hamburger.classList.toggle('hamburger-active');
-  nav.classList.toggle('sidebar-active');
-  content.classList.toggle('content-wrapper-active');
-  headerWrapper.classList.toggle('header-whitebar-active');
-  header.classList.toggle('header-active');
+  hamburger.classList.toggle("hamburger-active");
+  nav.classList.toggle("sidebar-active");
+  content.classList.toggle("content-wrapper-active");
+  headerWrapper.classList.toggle("header-whitebar-active");
+  header.classList.toggle("header-active");
+  sidebarOverlay.classList.toggle("sidebar-overlay-active");
 };
 
-hamburger.addEventListener('click', hamburgerClickHandler);
-
+hamburger.addEventListener("click", hamburgerClickHandler);
 
 for (let sidebarListItem of sidebarListItems) {
-  sidebarListItem.addEventListener('click', function () {
+  sidebarListItem.addEventListener("click", function() {
     const clickedElement = this;
-    const activeItems = document.querySelectorAll(
-      '.sidebar__list-item-active'
-    );
+    const activeItems = document.querySelectorAll(".sidebar__list-item-active");
 
     for (let activeItem of activeItems) {
-      activeItem.classList.remove('sidebar__list-item-active');
+      activeItem.classList.remove("sidebar__list-item-active");
     }
-    clickedElement.classList.add('sidebar__list-item-active');
+    clickedElement.classList.add("sidebar__list-item-active");
 
-    const clickedElementTag = clickedElement.getAttribute('data-tags');
+    const clickedElementTag = clickedElement.getAttribute("data-tags");
 
-    const windows = document.querySelectorAll('.' + clickedElementTag);
+    const windows = document.querySelectorAll("." + clickedElementTag);
 
     for (let window of allWindows) {
-      window.classList.remove('active');
+      window.classList.remove("active");
     }
 
     for (let window of windows) {
-      window.classList.add('active');
+      window.classList.add("active");
     }
   });
 }
@@ -48,83 +47,85 @@ for (let sidebarListItem of sidebarListItems) {
 //==========================
 
 function closeModal() {
-  document.getElementById('overlay').classList.remove('show');
+  document.getElementById("overlay").classList.remove("show");
 }
 
-document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
+document.querySelectorAll("#overlay .js--close-modal").forEach(function(btn) {
+  btn.addEventListener("click", function(e) {
     e.preventDefault();
     closeModal();
   });
 });
 
-document.querySelector('#overlay').addEventListener('click', function(e) {
-  if(e.target === this) {
+document.querySelector("#overlay").addEventListener("click", function(e) {
+  if (e.target === this) {
     closeModal();
   }
 });
 
-document.addEventListener('keyup', function(e) {
-  if(e.keyCode === 27) {
+document.addEventListener("keyup", function(e) {
+  if (e.keyCode === 27) {
     closeModal();
   }
 });
 
 function openModal(modal) {
-  document.querySelectorAll('#overlay > *').forEach(function(modal) {
-    modal.classList.remove('show');
+  document.querySelectorAll("#overlay > *").forEach(function(modal) {
+    modal.classList.remove("show");
   });
-  document.querySelector('#overlay').classList.add('show');
-  document.querySelector(modal).classList.add('show');
+  document.querySelector("#overlay").classList.add("show");
+  document.querySelector(modal).classList.add("show");
 }
 
-document.querySelector('.manager').addEventListener('click', function () {
-  openModal('#manager');
+document.querySelector(".manager").addEventListener("click", function() {
+  openModal("#manager");
 });
 
-document.querySelector('.login').addEventListener('click', function () {
-  openModal('#login');
+document.querySelector(".login").addEventListener("click", function() {
+  openModal("#login");
 });
 
-document.querySelector('.quit').addEventListener('click', function () {
-  openModal('#quit');
+document.querySelector(".quit").addEventListener("click", function() {
+  openModal("#quit");
 });
 
 // =========================
 // ======= CHART ==========
 //==========================
 
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById("myChart").getContext("2d");
 
 var chart = new Chart(ctx, {
   // 1
-  type: 'bar',
+  type: "bar",
   data: {
     // 2
     labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-      // 3
-      datasets: [{
-          // 4
-          label: "Signups",
-          // 5
-          backgroundColor: '#8DBEC8',
-          borderColor: '#8DBEC8',
-          // 6
-          data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+    // 3
+    datasets: [
+      {
+        // 4
+        label: "Signups",
+        // 5
+        backgroundColor: "#8DBEC8",
+        borderColor: "#8DBEC8",
+        // 6
+        data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88]
       },
       {
-          label: "FTD",
-          backgroundColor: '#F29E4E',
-          borderColor: '#F29E4E',
-          data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        label: "FTD",
+        backgroundColor: "#F29E4E",
+        borderColor: "#F29E4E",
+        data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76]
       },
       {
-          label: "Earned",
-          backgroundColor: '#71B374',
-          borderColor: '#71B374',
-          data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-          // 7
-          hidden: true,
-      }]
-  },
+        label: "Earned",
+        backgroundColor: "#71B374",
+        borderColor: "#71B374",
+        data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+        // 7
+        hidden: true
+      }
+    ]
+  }
 });
